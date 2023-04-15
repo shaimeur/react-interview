24244f1db21153d096eaeeea68ddfb8147965f46
shaimeur <haimeursaad@gmail.com>
2023-04-15 18:23:49 +0000

    adding pagination using the useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,44 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   const [myData, setMyData] = useState([]);
+  const [currentPage,setCurrentPage] = useState(1);
 
   const clickHundler = () => {
     console.log(count);
     return setCount(count + 1);
   };
 
-  useEffect(() => {
-    const url = `https://randomuser.me/api`;
-    fetch(url, {
-      method: "GET",
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data.results);
-        return setMyData(data.results);
-      })
-      .catch((err) => {
-        console.log(err);
+  const url = `https://randomuser.me/api`;
+
+  const fetchUserData = async (page) => {
+    try {
+      const res = await fetch(`${url}?page=${page}&results=10`, {
+        method: "GET",
       });
-  }, []);
+      const data = await res.json();
+      console.log(data.results);
+      return data.results;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData(currentPage).then((randomData) => {
+      setMyData(randomData);
+    });
+  }, [currentPage]);
+
+  const handleNextPage = () =>{
+    return setCurrentPage(currentPage + 1 );
+  }
+
+  const handlePrevPage = () =>{
+    if(currentPage > 1){
+
+      return setCurrentPage(currentPage - 1);
+    }
+  }
 
   return (
     <div className="App">
@@ -33,20 +49,25 @@ function App() {
       <br />
       <br />
       <br />
+      <p>the JSON Object in string format using JSON.stringify  </p>
+      {/* {JSON.stringify(myData)} */}
 
-      {myData.map((item) => {
-        return (
-          <div key={item.id}>
-            <img src={item.picture.large} alt="" />
-            <p>my name is  {item.name.first}</p>
-            <p> I'm a {item.gender}</p>
-            <p>my age is  {item.dob.age}</p>
-            <p> call me in {item.phone}</p>
-          </div>
-        );
-      })}
+      <br />
+      <br />
+      <br />
+      <br />
+      {myData.map((item) => (
+        <div key={item.login.uuid}>
+          <img src={item.picture.large} alt="" />
+          <p>Hi!  my name is {item.name.first} I'm {item.dob.age} </p>
+          <p>I live  {item.location.city} {item.location.country} </p>
+          <p>you can call me in my cell phone {item.phone}</p>
+
+        </div>
+      ))}
+      <button onClick={handlePrevPage}>Prev page</button>
+      <button onClick={handleNextPage}>Next Page</button>
     </div>
   );
 }
-
 export default App;
